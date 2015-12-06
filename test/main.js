f2b82ab799138f3a6fec207ca4ff332339cb7ec4
Nikhil Tilwalli <ntilwalli@gmail.com>
2015-12-06 12:48:42 -0500

    Update test descriptions

--- test/main.js
+++ test/main.js
@@ -6,7 +6,7 @@ import {VNode, diff, patch} from 'virtual-dom'
 
 // createMapOnElement tests
 test("createMapOnElement is a function", assert => {
-  assert.equal(typeof createMapOnElement, "function")
+  assert.equal(typeof createMapOnElement, "function", "createMapOnElement should be a function")
   assert.end()
 })
 
@@ -14,15 +14,15 @@ test("map DOM element and map instance are instantiated and attached to given el
   let element = document.createElement('div')
   const options = { zoomControl: false }
   createMapOnElement(element, "pk.eyJ1IjoibXJyZWRlYXJzIiwiYSI6IjQtVVRTZkEifQ.ef_cKBTmj8rSr7VypppZdg", new VNode('map', {options}))
-  assert.equal(element.mapDOM.tagName, 'MAP')
-  assert.equal(true, element.mapDOM.instance instanceof L.Map)
+  assert.equal(element.mapDOM.tagName, 'MAP', "property mapDOM should contain element with tagName 'MAP'")
+  assert.equal(true, element.mapDOM.instance instanceof L.Map, "property mapDOM should have have instance property that is an L.Map")
   assert.end()
 
 })
 
 // createElement tests
 test("createElement is a function", assert => {
-    assert.equal(typeof createElement, "function")
+    assert.equal(typeof createElement, "function", 'createElement should be a function')
     assert.end()
 })
 
@@ -31,15 +31,15 @@ test("render outputs the expected mapDOM element with properties and attributes"
 
   let dom = createElement(vdom)
 
-  assert.equal(dom.tagName, "TILELAYER")
-  assert.equal(dom.tile, "blah")
-  assert.deepEqual(dom.id, "someid")
+  assert.equal(dom.tagName, "TILELAYER", "should create element with tagName 'TILELAYER'")
+  assert.equal(dom.tile, "blah", "created element should have expected tile property value")
+  assert.deepEqual(dom.id, "someid", "created element should expected id property value")
   assert.end()
 })
 
 // patchRecursive tests
 test("patch is a function", assert => {
-    assert.equal(typeof patch, "function")
+    assert.equal(typeof patchRecursive, "function", 'patchRecursive should be a function')
     assert.end()
 })
 
@@ -49,11 +49,11 @@ test("patches mapDOM element to expected properties and attributes", assert => {
   createMapOnElement(element, "pk.eyJ1IjoibXJyZWRlYXJzIiwiYSI6IjQtVVRTZkEifQ.ef_cKBTmj8rSr7VypppZdg", firstVdom)
   let secondVdom = new VNode('map', {centerZoom: {zoom: 8, center: [5, 6]}})
   let dom = element.mapDOM
-  assert.equal(dom.tagName, "MAP")
-  assert.deepEqual(dom.centerZoom, {zoom: 7, center:[4,5]})
+  assert.equal(dom.tagName, "MAP", "should have root element with tagName 'MAP' ")
+  assert.deepEqual(dom.centerZoom, {zoom: 7, center:[4,5]}, "should have passed initial centerZoom value")
   let patches = diff(firstVdom, secondVdom)
 
   let newRoot = patch(element, patches, {render: createElement, patch: patchRecursive})
-  assert.deepEqual(dom.centerZoom, {zoom: 8, center:[5,6]})
+  assert.deepEqual(dom.centerZoom, {zoom: 8, center:[5,6]}, "should have patched centerZoom value")
   assert.end()
 })
